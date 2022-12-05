4b5e6b7d069a9088fb9d6dbfa0b1d9eb9f3be721
Petr Laštovička <krasik.peta@seznam.cz>
2022-12-05 10:32:48 +0100

    day05: Part 01

---
+++ src/main/kotlin/day05/Main.kt
@@ -0,0 +1,87 @@
+package day05
+
+import java.util.Stack
+
+data class Operation(
+    val count: Int,
+    val from: Int,
+    val to: Int,
+)
+
+typealias InputRow = List<Char?>
+
+private fun String.parseInput(): Pair<List<InputRow>, List<Operation>> {
+    val separator = lineSequence().indexOfFirst { it.isBlank() }
+
+    val stacks = lineSequence().take(separator - 1)
+        .map { line ->
+            line.chunked(4)
+        }
+        .map { chunks ->
+            chunks.map { chunk ->
+                chunk[1].takeIf { it.isLetter() }
+            }
+        }.toList()
+
+    val instructions = lineSequence().drop(separator + 1)
+        .map { line ->
+            line.split(" ").filter { it.first().isDigit() }.map { it.toInt() }
+        }
+        .map { (count, from, to) ->
+            Operation(count, from - 1, to - 1)
+        }.toList()
+
+    return stacks to instructions
+}
+
+fun List<InputRow>.toStacks(): List<Stack<Char>> {
+    val stacks = last().map { Stack<Char>() }
+
+    reversed().forEach { row ->
+        row.forEachIndexed { index, payload ->
+            payload?.let {
+                stacks[index] += it
+            }
+        }
+    }
+
+    return stacks
+}
+
+fun List<Stack<Char>>.applyOperation(operation: Operation) {
+    repeat(operation.count) {
+        this[operation.to] += this[operation.from].peek()
+        this[operation.from].pop()
+    }
+}
+
+fun String.part01() : String {
+    val (stackInput, operations) = parseInput()
+    val stacks = stackInput.toStacks()
+
+    operations.forEach {
+        stacks.applyOperation(it)
+    }
+
+    return stacks.map { it.peek() }.joinToString("")
+}
+
+fun main() {
+    listOf(
+        TEST_INPUT,
+        InputLoader.loadInput("day05"),
+    ).forEach { input ->
+        println(input.part01())
+    }
+}
+
+private val TEST_INPUT = """
+        [D]    
+    [N] [C]    
+    [Z] [M] [P]
+     1   2   3 
+    
+    move 1 from 2 to 1
+    move 3 from 1 to 3
+    move 2 from 2 to 1
+    move 1 from 1 to 2""".trimIndent()
