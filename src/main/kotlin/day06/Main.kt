723d7079a5ad75c1182fbc39ec1a974f0fd5a4cb
Petr Laštovička <krasik.peta@seznam.cz>
2022-12-06 17:52:26 +0100

    day06: Part 02

--- src/main/kotlin/day06/Main.kt
+++ src/main/kotlin/day06/Main.kt
@@ -3,37 +3,44 @@ package day06
 import InputLoader
 import kotlinx.collections.immutable.persistentListOf
 
-private const val TAG_LENGTH = 4
-
-private fun String.part01() : Int {
+private fun String.findMarker(length: Int) : Int {
     var queue = persistentListOf<Char>()
 
     forEachIndexed { index, _ ->
         val c = get(index)
         queue = queue.add(c)
 
-        if (queue.toSet().size == TAG_LENGTH)
+        if (queue.toSet().size == length)
             return index + 1
 
-        if (queue.size == 4)
+        if (queue.size == length)
             queue = queue.removeAt(0)
     }
 
     return -1
 }
 
+private fun String.part01() : Int =
+    findMarker(4)
+
+private fun String.part02() : Int =
+    findMarker(14)
+
 fun main() {
     testInputs.forEach {
-        require(it.first.part01() == it.second)
+        require(it.first.part01() == it.second.first)
+        require(it.first.part02() == it.second.second)
     }
+
     val input = InputLoader.loadInput("day06")
     println(input.part01())
+    println(input.part02())
 }
 
 private val testInputs = listOf(
-    "mjqjpqmgbljsphdztnvjfqwrcgsmlb" to 7,
-    "bvwbjplbgvbhsrlpgdmjqwftvncz" to 5,
-    "nppdvjthqldpwncqszvftbrmjlhg" to 6,
-    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" to 10,
-    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" to 11,
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb" to (7 to 19),
+    "bvwbjplbgvbhsrlpgdmjqwftvncz" to (5 to 23),
+    "nppdvjthqldpwncqszvftbrmjlhg" to (6 to 23),
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" to (10 to 29),
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" to (11 to 26),
 )
