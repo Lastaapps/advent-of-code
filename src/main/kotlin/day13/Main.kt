8412adfb5f67c9303afa82b3acb24dbaa72da093
Petr Laštovička <krasik.peta@seznam.cz>
2022-12-13 10:05:47 +0100

    day13: Part 02

--- src/main/kotlin/day13/Main.kt
+++ src/main/kotlin/day13/Main.kt
@@ -2,6 +2,7 @@ package day13
 
 import InputLoader
 import io.kotest.matchers.shouldBe
+import kotlinx.collections.immutable.toPersistentList
 import kotlin.math.min
 
 private sealed interface Elem : Comparable<Elem> {
@@ -9,7 +10,7 @@ private sealed interface Elem : Comparable<Elem> {
     value class EList(val value: List<Elem>) : Elem {
         override fun compareTo(other: Elem): Int {
             return when (other) {
-                is EInt -> compareTo(EList(listOf(other)))
+                is EInt -> compareTo(EList(other))
                 is EList -> {
                     for (i in 0 until min(value.size, other.value.size)) {
                         value[i].compareTo(other.value[i]).let {
@@ -20,6 +21,8 @@ private sealed interface Elem : Comparable<Elem> {
                 }
             }
         }
+
+        constructor(value: Elem) : this(listOf(value))
     }
 
     @JvmInline
@@ -27,15 +30,14 @@ private sealed interface Elem : Comparable<Elem> {
         override fun compareTo(other: Elem): Int =
             when (other) {
                 is EInt -> value.compareTo(other.value)
-                is EList -> EList(listOf(this)).compareTo(other)
+                is EList -> EList(this).compareTo(other)
             }
     }
 }
 
-private fun ListIterator<Char>.parseDigit() : Elem.EInt =
+private fun ListIterator<Char>.parseDigit(): Elem.EInt =
     buildString {
-        while(true) {
-
+        while (true) {
             when (val char = next()) {
                 in '0'..'9' -> append(char)
                 else -> break
@@ -46,27 +48,29 @@ private fun ListIterator<Char>.parseDigit() : Elem.EInt =
         return Elem.EInt(it.toInt())
     }
 
-private fun ListIterator<Char>.parseList() : Elem.EList {
+private fun ListIterator<Char>.parseList(): Elem.EList {
     val items = mutableListOf<Elem>()
     next() shouldBe '['
 
     while (true) {
-        when(next()) {
+        when (val char = next()) {
             '[' -> {
                 previous()
                 items.add(parseList())
             }
+
             ']' -> return Elem.EList(items)
             ',' -> {}
             in '0'..'9' -> {
                 previous()
                 items.add(parseDigit())
             }
+            else -> error("Unexpected character $char")
         }
     }
 }
 
-private fun String.parseInput() =
+private fun String.parseInputPairs() =
     lines()
         .chunked(3)
         .map { chunk ->
@@ -75,13 +79,32 @@ private fun String.parseInput() =
         }
 
 private fun String.part01(): Int =
-    parseInput()
+    parseInputPairs()
         .mapIndexed { index, (left, right) ->
-        if (left.compareTo(right) != 1) index + 1
-        else 0
-    }.sum()
+            if (left.compareTo(right) != 1) index + 1
+            else 0
+        }.sum()
 
-private fun String.part02(): Int = 0
+
+private fun String.parseInputLines() =
+    lineSequence()
+        .filter { it.isNotBlank() }
+        .map { line ->
+            line.toList().listIterator().parseList()
+        }
+
+private fun String.part02(): Int =
+    parseInputLines().let { input ->
+        val marks = listOf(
+            Elem.EList(Elem.EList(Elem.EInt(2))),
+            Elem.EList(Elem.EList(Elem.EInt(6))),
+        )
+        (input + marks).sorted().toList().let { sorted ->
+            marks.map { mark ->
+                sorted.binarySearch(mark) + 1
+            }.let { (f, s) -> f * s }
+        }
+    }
 
 fun main() {
     testInput.part01() shouldBe PART_01_RES
@@ -119,4 +142,4 @@ private val testInput = """
 """.trimIndent()
 
 private const val PART_01_RES = 13
-private const val PART_02_RES = 0
+private const val PART_02_RES = 140
